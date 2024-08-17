d5ec7d110c8456aa9ab51942608faeff3204c05c
Shubham Kumar <imshubhamkr50@gmail.com>
2024-08-17 09:17:44 +0530

    Sanity & Schemas setup

---
+++ sanity/env.ts
@@ -0,0 +1,20 @@
+export const apiVersion =
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-08-16'
+
+export const dataset = assertValue(
+  "production",
+  'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
+)
+
+export const projectId = assertValue(
+  "j8kmcwec",
+  'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
+)
+
+function assertValue<T>(v: T | undefined, errorMessage: string): T {
+  if (v === undefined) {
+    throw new Error(errorMessage)
+  }
+
+  return v
+}
