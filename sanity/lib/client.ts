be1a2519c3de8275d701d15b87edcbc2e6997691
Shubham Kumar <imshubhamkr50@gmail.com>
2024-08-17 15:35:47 +0530

    Created hero banner for home page

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,10 +1,14 @@
 import { createClient } from 'next-sanity'
+import imageUrlBuilder from '@sanity/image-url';
+import createImageUrlBuilder from '@sanity/image-url'
+import { SanityImageSource } from "@sanity/image-url/lib/types/types";
 
 import { apiVersion, dataset, projectId } from '../env'
 
 export const client = createClient({
-  projectId,
-  dataset,
-  apiVersion,
+  projectId: "j8kmcwec",
+  dataset: "production",
+  apiVersion: "2024-08-16",
   useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
+  token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
 })
